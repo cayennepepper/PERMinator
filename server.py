4f7f0e4a7f9fca954cb34ae7a227917ceab28347
Sarah Jundt <sarahjundt@Sarahs-MacBook-Pro-2.local>
2014-10-25 10:19:25 -0700

    server has pages for students and professors

--- server.py
+++ server.py
@@ -5,9 +5,29 @@ app = Flask(__name__)
 def index_page():
     return 'Index!'
 
-@app.route('/perms')
-def  perm_page():
-	return 'PERM'
+@app.route('/professor/<pid>')
+def  professor_home(pid): 
+	return 'HOME PAGE FOR PROFESSOR '+pid
+
+@app.route('/student/<sid>')
+def  student_home(sid):
+	return 'PERMS FOR '+sid
+
+@app.route('/myCourse/<cid>')
+def  course(cid):
+	return 'COURSE '+cid
+
+@app.route('/deptCourse/<cid>')
+def  dept_course(cid):
+	return 'DEPT COURSE '+cid
+
+@app.route('/student/<sid>/edit')
+def  edit_perms(sid):
+	return 'EDIT PERMS '
+
+@app.route('/student/<sid>/submit')
+def  submit_perms(sid):
+	return 'SUBMIT PERMS '
 
 if __name__ == '__main__':
     app.run()
