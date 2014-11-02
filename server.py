3da026f7f82647b6bd8b89ceeff1c8f97c2b6b88
Sarah Jundt <sarahjundt@Sarahs-MacBook-Pro-5.local>
2014-11-02 11:56:00 -0800

    init db uses flask's version of sqlalchemy

--- server.py
+++ server.py
@@ -1,7 +1,32 @@
 from flask import Flask
 from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#sets up db connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:@localhost/PERMinator?charset=utf8&use_unicode=0'
+db = SQLAlchemy(app)
+
+#DB SCHEMA: STUDENT MODEL
+class Student(db.Model):
+  id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+  sFirstName = db.Column(db.String(50))
+  sLastName = db.Column(db.String(50))
+  year = db.Column(db.Integer)
+  college = db.Column(db.String(6))
+  sEmail = db.Column(db.String(50))
+
+  def __init__(self, id, sFirstName, sLastName, year, college, sEmail):
+    self.id = id
+    self.sFirstName = sFirstName
+    self.sLastName = sLastName
+    self.year = year
+    self.college = college
+    self.sEmail = sEmail
+    
+  def __repr__(self):
+    return "<Student(id='%s', sFirstName='%s', sLastName='%s', year='%s', college='%s', sEmail='%s')>" % (
+      self.id, self.sFirstName, self.sLastName, self.year, self.college,self.sEmail)
 
 @app.route('/')
 def index_page():
