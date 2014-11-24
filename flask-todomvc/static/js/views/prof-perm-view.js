f7dd743a966db69e5f82e7a456222a4ae456aaff
Sarah Jundt <sarahjundt@Sarahs-MacBook-Pro-41.local>
2014-11-24 13:50:15 -0800

    professor perm view front-end for changing exp date

--- flask-todomvc/static/js/views/prof-perm-view.js
+++ flask-todomvc/static/js/views/prof-perm-view.js
@@ -13,15 +13,69 @@ var app = app || {};
 
 		// The DOM events specific to an item.
 		events: {
-			'dblclick expdate': 'edit',
-			'keypress .edit': 'updateOnEnter',
-			'keydown .edit': 'revertOnEscape',
+			'click #expdate': 'edit',
+			'keypress': 'updateOnEnter',
+			'keydown': 'revertOnEscape',
+		},
+
+		initialize: function() {
+			this.listenTo(this.model, 'change', this.render);
+		},
+
+		// If you hit `enter`, we're through editing the item.
+		updateOnEnter: function (e) {
+			if (e.which === ENTER_KEY) {
+				this.close();
+			}
+		},
+
+		// If you're pressing `escape` we revert your change by simply leaving
+		// the `editing` state.
+		revertOnEscape: function (e) {
+			if (e.which === ESC_KEY) {
+				this.$el.removeClass('editing');
+			}
+		},
+
+		// Switch this view into `"editing"` mode, displaying the input field.
+		edit: function () {
+			this.$el.addClass('editing');
+			// this.$expDate.focus();
 		},
 
 		// Re-render the titles of the todo item.
 		render: function () {
 			this.$el.html(this.template(this.model.toJSON()));
 			return this;
+		},
+
+		// Close the `"editing"` mode, saving changes to the todo.
+		close: function () {
+			var value = $('#expdate').val();
+			var trimmedValue = value.trim();
+
+			// We don't want to handle blur events from an item that is no
+			// longer being edited. Relying on the CSS class here has the
+			// benefit of us not having to maintain state in the DOM and the
+			// JavaScript logic.
+			if (!this.$el.hasClass('editing')) {
+				return;
+			}
+
+			if (trimmedValue) {
+				this.model.save({ expirationTime: trimmedValue });
+
+				if (value !== trimmedValue) {
+					// Model values changes consisting of whitespaces only are not causing change to be triggered
+					// Therefore we've to compare untrimmed version with a trimmed one to chech whether anything changed
+					// And if yes, we've to trigger change event ourselves
+					this.model.trigger('change');
+				}
+			} else {
+				this.clear();
+			}
+
+			this.$el.removeClass('editing');
 		},
 	});
 })(jQuery);
